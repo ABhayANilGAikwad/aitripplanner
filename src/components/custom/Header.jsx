85835692eaf71b3ab89d5de08a9478c3881a5084
Saurabh Gadhe <gadhesaurabh74@gmail.com>
2025-04-06 12:16:45 +0530

    some issues related to sign in are solved

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -6,7 +6,9 @@ const Header = () => {
     <div className='p-3 px-5 shadow-xl flex justify-between items-center'>
       <img src="logo.png" alt=""  className='w-16 h-15'/>
       <div>
-        <Button>Sign in</Button>
+      <Link to={"/create-trip"}>
+            <Button>Get Started, it's Free</Button>
+      </Link>
       </div>
     </div>
   )
