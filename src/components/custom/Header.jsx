35ff77f58f9a2d5d0e2ca2ddb314424d48965299
Saurabh Gadhe <gadhesaurabh74@gmail.com>
2025-04-07 13:57:01 +0530

    issue solvede for deployemnt

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -9,9 +9,7 @@ const Header = () => {
     <div className='p-3 px-5 shadow-xl flex justify-between items-center'>
       <img src="logo.png" alt=""  className='w-16 h-15'/>
       <div>
-      <Link to={"/create-trip"}>
-            <Button>Get Started, it's Free</Button>
-      </Link>
+      
       </div>
     </div>
   )
